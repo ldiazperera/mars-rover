3bfe941f8b82988d98448c5ef8da1c6d261ef7a0
Lucas Diaz Perera <Lucas.X.Diaz.-ND@disney.com>
2018-09-17 12:38:55 -0300

    Photo list. Photos mocked

---
+++ src/app/mock-photos.ts
@@ -0,0 +1,32 @@
+import { Photo } from './photo';
+
+export const PHOTOS: Photo[] = [
+    {
+      title: 'Picture one',
+      date: '01/01/1990'
+    },
+    {
+      title: 'Picture two',
+      date: '02/01/1990'
+    },
+    {
+      title: 'Picture three',
+      date: '03/01/1990'
+    },
+    {
+      title: 'Picture four',
+      date: '04/01/1990'
+    },
+    {
+      title: 'Picture five',
+      date: '05/01/1990'
+    },
+    {
+        title: 'Picture six',
+        date: '06/01/1990'
+    },
+    {
+        title: 'Picture seven',
+        date: '07/01/1990'
+    }
+];
--- src/app/photo-list/photo-list.component.ts
+++ src/app/photo-list/photo-list.component.ts
@@ -1,21 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { Photo } from '../photo';
+import { PHOTOS } from '../mock-photos';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
   styleUrls: ['./photo-list.component.css']
 })
+
 export class PhotoListComponent implements OnInit {
 
-  photo: Photo = {
-    title: 'Picture of the day',
-    date: '01/01/1990'
-  };
+  photos = PHOTOS;
+  selectedPhoto: Photo;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelect(photo: Photo): void {
+    this.selectedPhoto = photo;
   }
 
 }
